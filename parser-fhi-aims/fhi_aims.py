2043efd2a3ba2a988e4013be9801fe253ff30bb9
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2015-11-24 20:03:00 +0100

    first run with the json streaming interface

--- parser-fhi-aims/fhi_aims.py
+++ parser-fhi-aims/fhi_aims.py
@@ -1,11 +1,19 @@
 #!/bin/env python
 import setup_paths
-import nomadcore.parser_backend
-from nomadcore.local_meta_info import loadJsonFile
-import os, os.path
+from nomadcore.parser_backend import JsonParseEventsWriterBackend
+from nomadcore.local_meta_info import loadJsonFile, InfoKindEl
+from nomadcore.parse_streamed_dicts import ParseStreamedDicts
+import os, os.path, sys
+
+def parseFile(path, backend):
+    backend.startedParsingSession(os.path.abspath(path),
+                                  {"name":"fhi-aims-parser", "version": "1.0"})
+    backend.finishedParsingSession(os.path.abspath(path),
+                                   {"name":"fhi-aims-parser", "version": "1.0"})
+
 
 metaInfoPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../../../nomad-meta-info/nomad_meta_info/fhi_aims.nomadmetainfo.json"))
-metaInfoEnv = loadJsonFile(filePath = metaInfoPath, dependencyLoader = None, extraArgsHandling = InfoKindEl.ADD_EXTRA_ARGS, uri = None)
+metaInfoEnv, warnings = loadJsonFile(filePath = metaInfoPath, dependencyLoader = None, extraArgsHandling = InfoKindEl.ADD_EXTRA_ARGS, uri = None)
 
 if __name__ == "__main__":
     usage = """{exeName} [--meta-info] [--help] [--specialize] [--stream] [path/toFile]
@@ -15,34 +23,64 @@ if __name__ == "__main__":
     --specialize expects inclusion and exclusion of meta info to parse via a json disctionary on stdin
     --stream expects the files to parse via dictionary on stdin
 
-    If a path to a file is given this is parsed""".format(exeName = os.path.basename(sys.argv.get(0,"fhi_aims.py")))
+    If a path to a file is given this is parsed
+""".format(exeName = os.path.basename(sys.argv[0] if len(sys.argv) > 0 else "fhi_aims.py"))
     outF = sys.stdout
     metaInfo = False
     specialize = False
     stream = False
     fileToParse = None
     ii = 1
-    while ii < sys.argv:
+    while ii < len(sys.argv):
         arg = sys.argv[ii]
+        ii += 1
         if arg == "--meta-info":
             metaInfo = True
         elif arg == "--help":
-            print usage
+            sys.stderr.write(usage)
             sys.exit(0)
         elif arg == "--specialize":
             specialize = True
         elif arg == "--stream":
             stream = True
         elif not fileToParse:
-            fileToParse
+            fileToParse = arg
+        else:
+            sys.stderr.write("unexpected argument " + arg + "\n")
+            sys.stderr.write(usage)
+            sys.exit(1)
+    outF.write("[")
+    writeComma = False
     if metaInfo:
+        if writeComma:
+            outF.write(", ")
+        else:
+            writeComma = True
         metaInfoEnv.embedDeps()
-        metaInfoEnv.serialize(self, outF.write, subGids = True, selfGid = True):
+        metaInfoEnv.serialize(self, outF.write, subGids = True, selfGid = True)
         outF.flush()
-    dictReader = DictReader(sys.stdin)
+    dictReader = ParseStreamedDicts(sys.stdin)
     toOuput = list(metaInfoEnv.infoKinds.keys())
+    backend = JsonParseEventsWriterBackend(metaInfoEnv, sys.stdout)
     if specialize:
         specializationInfo = dictReader.readNextDict()
         if specializationInfo is None or specializationInfo.get("type", "") != "nomad_parser_specialization_1_0":
             raise Exception("expected a nomad_parser_specialization_1_0 as first dictionary, got " + json.dumps(specializationInfo))
         specializationInfo
+    if fileToParse:
+        if writeComma:
+            outF.write(", ")
+        else:
+            writeComma = True
+        parseFile(fileToParse, backend)
+    if stream:
+        while True:
+            if writeComma:
+                outF.write(", ")
+            else:
+                writeComma = True
+            toRead = dictReader.readNextDict()
+            if toRead is None:
+                break
+            parseFile(toRead['mainFile'])
+    outF.write("]\n")
