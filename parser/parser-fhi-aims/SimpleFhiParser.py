968409ccaaf314ec7577fd219e6e7eb833d1d5d5
Franz Knuth <knuth@fhi-berlin.mpg.de>
2015-12-18 15:07:28 +0100

    added units and rewrote eigenvalue parsing with triggers

--- parser/parser-fhi-aims/SimpleFhiParser.py
+++ parser/parser-fhi-aims/SimpleFhiParser.py
@@ -5,91 +5,110 @@ from nomadcore.local_meta_info import loadJsonFile, InfoKindEl
 from nomadcore.caching_backend import CachingLevel
 import re, os, sys, json, logging
 
-# Global names
-ev_number = 'ev_number'
-ev_occupation = 'ev_occupation'
-ev_value = 'ev_value'
+class FhiAimsParserContext(object):
+    def __init__(self):
+        # start with -1 since zeroth iteration is the initialization
+        self.scfIterNr = -1
+        self.eigenvalues_occupation = []
+        self.eigenvalues_eigenvalues = []
 
-# This class is used to ensure consistent naming for the parsing of the eigenvalues and contains the compiled regular expression
-class ParseEigenvaluesNamesAndRe:
-    def __init__(self, metaInfoEnv, metaNameEvNumber, metaNameEvOccupation, metaNameEvValue):
-        # Create dictionaty and check metadata
-        self.metaName = {ev_number: metaNameEvNumber, ev_occupation: metaNameEvOccupation, ev_value: metaNameEvValue}
-        for metaName in self.metaName.values():
-            if not metaName in metaInfoEnv:
-                sys.stderr.write("Meta info %s is not in the meta info, but is used for the parsing of eigenvalues.\n" % metaName)
-                sys.stderr.write("If it was not a typo, you might want to add a meta info.\n")
-                sys.exit(1)
-        self.variable = {ev_number: 0, ev_occupation: [], ev_value: []}
-        self.re = re.compile(r"\s*(?P<" + ev_number + r">[0-9]+)\s*(?P<" + ev_occupation + r">[0-9.eEdD]+)\s*(?P<" + ev_value + r">[-+0-9.eEdD]+)\s*[-+0-9.eEdD]+")
+    def onClose_section_single_point_evaluation(self, backend, gIndex, section):
+        """trigger called when section_single_point_evaluation is closed"""
+        backend.addValue('scf_dft_number_of_iterations', self.scfIterNr)
+        # start with -1 since zeroth iteration is the initialization
+        self.scfIterNr = -1
 
-# adHoc function which parses the list of eigenvalues
-def parseEigenvalues(parser):
-    # reset variables
-    evUtility.variable[ev_number] = 0
-    evUtility.variable[ev_occupation] = []
-    evUtility.variable[ev_value] = []
-    # parse line by line until no match is found
-    while True:
-        line = parser.fIn.readline()
-        if not line:
-            break
-        # search for match with regular expression
-        m = evUtility.re.match(line)
-        if not m:
-            # do not eat last read line
-            parser.fIn.pushbackLine(line)
-            break
-        # extract values
-        evUtility.variable[ev_number] = int(m.groupdict()[ev_number])
-        evUtility.variable[ev_occupation].append(float(m.groupdict()[ev_occupation]))
-        evUtility.variable[ev_value].append(float(m.groupdict()[ev_value]))
-    # write data if at least one eigenvalue was found
-    if evUtility.variable[ev_number] > 0:
-        #parser.backend.addValue(evUtility.metaName[ev_number], evUtility.variable[ev_number])
-        parser.backend.addArrayValues(evUtility.metaName[ev_occupation], np.asarray(evUtility.variable[ev_occupation]))
-        parser.backend.addArrayValues(evUtility.metaName[ev_value], np.asarray(evUtility.variable[ev_value]))
-    return None
+    def onClose_section_scf_iteration(self, backend, gIndex, section):
+        """trigger called when section_scf_iteration is closed"""
+        self.scfIterNr += 1
+
+    def onClose_fhi_aims_section_eigenvalues_list(self, backend, gIndex, section):
+        """trigger called when _aims_section_eigenvalues_list is closed"""
+        # get cached values for occupation and eigenvalues
+        occ = section.simpleValues['fhi_aims_eigenvalue_occupation']
+        ev = section.simpleValues['fhi_aims_eigenvalue_eigenvalue']
+        # check for consistent list length of obtained values
+        if len(occ) != len(ev):
+            raise Exception("Found %d eigenvalue occupations but %d eigenvalues." % (len(occ), len(ev)))
+        if self.eigenvalues_occupation:
+            logging.getLogger("nomadcore.parsing").info("YYYY %s", self.eigenvalues_occupation)
+            if len(self.eigenvalues_occupation[-1]) != len(occ):
+                raise Exception("Inconsistent number of eigenvalue occupations found in SCF cycle %d." % self.scfIterNr + 1)
+        if self.eigenvalues_eigenvalues:
+            if len(self.eigenvalues_eigenvalues[-1]) != len(ev):
+                raise Exception("Inconsistent number of eigenvalues found in SCF cycle %d." % self.scfIterNr + 1)
+        # append values for current k-point
+        self.eigenvalues_occupation.append(occ)
+        self.eigenvalues_eigenvalues.append(ev)
+
+    def onClose_section_eigenvalues(self, backend, gIndex, section):
+        """trigger called when section_eigenvalues is closed"""
+        backend.addArrayValues('eigenvalues_occupation', np.asarray(self.eigenvalues_occupation))
+        backend.addArrayValues('eigenvalues_eigenvalues', np.asarray(self.eigenvalues_eigenvalues))
+        self.eigenvalues_occupation = []
+        self.eigenvalues_eigenvalues = []
 
 # description of the input
 controlInSubMatchers = []
 geometryInSubMatchers = []
 
-# Eigenvalues need to be done: k-points, spin
-# submatcher for eigenvalues
-EigenvaluesSubMatcher = SimpleMatcher(name = 'Eigenvalues',
-              startReStr = r"\s*Writing Kohn-Sham eigenvalues\.",
-              sections = ['section_eigenvalues'],
+# TODO k-point extraction
+# submatcher for eigenvalue list
+EigenvaluesListSubMatcher =  SimpleMatcher(name = 'EigenValuesLists',
+              startReStr = r"\s*State\s*Occupation\s*Eigenvalue *\[Ha\]\s*Eigenvalue *\[eV\]",
+              sections = ['fhi_aims_section_eigenvalues_list'],
               subMatchers = [
-  SimpleMatcher(r"\s*K-point:\s*(?P<eigenvalues_kpoints_number>[0-9]+) *at\s*.*\s*\(in units of recip\. lattice\)"),
-  SimpleMatcher(startReStr = r"\s*State\s*Occupation\s*Eigenvalue *\[Ha\]\s*Eigenvalue *\[eV\]"),
-  SimpleMatcher(startReStr = r"\s*[0-9]+\s*[0-9.eEdD]+\s*[-+0-9.eEdD]+\s*[-+0-9.eEdD]+",
-                forwardMatch = True,
-                adHoc = parseEigenvalues)
+  SimpleMatcher(startReStr = r"\s*[0-9]+\s*(?P<fhi_aims_eigenvalue_occupation>[0-9.eEdD]+)\s*(?P<fhi_aims_eigenvalue_eigenvalue__hartree>[-+0-9.eEdD]+)\s*[-+0-9.eEdD]+", repeats = True)
+                  ])
+# submatcher for eigenvalue group
+EigenvaluesGroupSubMatcher = SimpleMatcher(name = 'EigenvaluesGroup',
+              startReStr = r"\s*Writing Kohn-Sham eigenvalues\.",
+              sections = ['section_eigenvalues_group'],
+              forwardMatch = True,
+              subMatchers = [
+  SimpleMatcher(startReStr = r"\s*(?:Writing Kohn-Sham eigenvalues\.|Spin-down eigenvalues:)",
+                repeats = True,
+                subMatchers = [
+    SimpleMatcher(r"\s*Spin-up eigenvalues:"),
+    # for periodic calculations
+    SimpleMatcher(name = 'EigenValuesKPoints',
+                  sections = ['section_eigenvalues'],
+                  startReStr = r"\s*K-point:\s*[0-9]+\s*at\s*.*\s*\(in units of recip\. lattice\)",
+                  repeats = True,
+                  subMatchers = [EigenvaluesListSubMatcher]
+                  ),
+    # for non-periodic calculations
+    SimpleMatcher(name = 'EigenValuesKPoints',
+                  sections = ['section_eigenvalues'],
+                  startReStr = r"\s*State\s*Occupation\s*Eigenvalue *\[Ha\]\s*Eigenvalue *\[eV\]",
+                  forwardMatch = True,
+                  subMatchers = [EigenvaluesListSubMatcher.copy()] # need copy since SubMatcher already used above
+                  ),
+                ])
               ])
 # submatcher for total energy components
 TotalEnergyScfSubMatcher = SimpleMatcher(name = 'TotalEnergyScf',
               startReStr = r"\s*Total energy components:",
               subMatchers = [
   SimpleMatcher(r"\s*\|\s*Sum of eigenvalues\s*:\s*(?P<energy_sum_eigenvalues_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*XC energy correction\s*:\s*(?P<energy_XC_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ eV"),
-  SimpleMatcher(r"\s*\|\s*XC potential correction\s*:\s*(?P<energy_XC_potential_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*Free-atom electrostatic energy\s*:\s*(?P<fhi_aims_energy_electrostatic_free_atom_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*Hartree energy correction\s*:\s*(?P<energy_correction_hartree_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*Entropy correction\s*:\s*(?P<energy_correction_entropy_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*van der Waals energy corr\.\s*:\s*(?P<energy_van_der_Waals_value>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*XC energy correction\s*:\s*(?P<energy_XC_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ eV"),
+  SimpleMatcher(r"\s*\|\s*XC potential correction\s*:\s*(?P<energy_XC_potential_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Free-atom electrostatic energy\s*:\s*(?P<fhi_aims_energy_electrostatic_free_atom_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Hartree energy correction\s*:\s*(?P<energy_correction_hartree_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Entropy correction\s*:\s*(?P<energy_correction_entropy_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*van der Waals energy corr\.\s*:\s*(?P<energy_van_der_Waals_value__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
   SimpleMatcher(r"\s*\|\s*-{20}-*", weak = True),
-  SimpleMatcher(r"\s*\|\s*Total energy\s*:\s*(?P<energy_total_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*Total energy, T -> 0\s*:\s*(?P<energy_total_T0_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*Electronic free energy\s*:\s*(?P<energy_free_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Total energy\s*:\s*(?P<energy_total_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Total energy, T -> 0\s*:\s*(?P<energy_total_T0_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Electronic free energy\s*:\s*(?P<energy_free_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
   SimpleMatcher(r"\s*Derived energy quantities:"),
-  SimpleMatcher(r"\s*\|\s*Kinetic energy\s*:\s*(?P<electronic_kinetic_energy_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*Electrostatic energy\s*:\s*(?P<energy_electrostatic_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Kinetic energy\s*:\s*(?P<electronic_kinetic_energy_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Electrostatic energy\s*:\s*(?P<energy_electrostatic_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
   SimpleMatcher(r"\s*\|\s*Energy correction for multipole"),
-  SimpleMatcher(r"\s*\|\s*error in Hartree potential\s*:\s*(?P<energy_hartree_error_scf_iteration>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
-  SimpleMatcher(r"\s*\|\s*Sum of eigenvalues per atom\s*:\s*(?P<energy_sum_eigenvalues_per_atom_scf_iteration>[-+0-9.eEdD]+) *eV"),
-  SimpleMatcher(r"\s*\|\s*Total energy \(T->0\) per atom\s*:\s*(?P<energy_total_T0_per_atom_scf_iteration>[-+0-9.eEdD]+) *eV"),
-  SimpleMatcher(r"\s*\|\s*Electronic free energy per atom\s*:\s*(?P<energy_free_per_atom_scf_iteration>[-+0-9.eEdD]+) *eV"),
+  SimpleMatcher(r"\s*\|\s*error in Hartree potential\s*:\s*(?P<energy_hartree_error_scf_iteration__hartree>[-+0-9.eEdD]+) *Ha\s*[-+0-9.eEdD]+ *eV"),
+  SimpleMatcher(r"\s*\|\s*Sum of eigenvalues per atom\s*:\s*(?P<energy_sum_eigenvalues_per_atom_scf_iteration__eV>[-+0-9.eEdD]+) *eV"),
+  SimpleMatcher(r"\s*\|\s*Total energy \(T->0\) per atom\s*:\s*(?P<energy_total_T0_per_atom_scf_iteration__eV>[-+0-9.eEdD]+) *eV"),
+  SimpleMatcher(r"\s*\|\s*Electronic free energy per atom\s*:\s*(?P<energy_free_per_atom_scf_iteration__eV>[-+0-9.eEdD]+) *eV"),
               ])
 
 # main Parser
@@ -152,25 +171,31 @@ mainFileDescription = SimpleMatcher(name = 'root',
                     sections = ['section_scf_iteration'],
                     subMatchers = [
         SimpleMatcher(r"\s*Date *: *(?P<fhi_aims_scf_date_start>[-.0-9/]+) *, *Time *: *(?P<fhi_aims_scf_time_start>[-+0-9.eEdD]+)"),
-        EigenvaluesSubMatcher,
+        EigenvaluesGroupSubMatcher,
         TotalEnergyScfSubMatcher
                      ]),
       SimpleMatcher(name = 'ScfIteration',
-                    startReStr = r"\s*Begin self-consistency iteration #\s*(?P<scf_dft_number_of_iterations>[0-9]+)",
+                    startReStr = r"\s*Begin self-consistency iteration #\s*[0-9]+",
                     sections = ['section_scf_iteration'],
                     repeats = True,
                     subMatchers = [
         SimpleMatcher(r"\s*Date *: *(?P<fhi_aims_scf_date_start>[-.0-9/]+) *, *Time *: *(?P<fhi_aims_scf_time_start>[-+0-9.eEdD]+)"),
-        EigenvaluesSubMatcher.copy(), # need copy since SubMatcher already used for ScfInitialization
+        EigenvaluesGroupSubMatcher.copy(), # need copy since SubMatcher already used for ScfInitialization
         TotalEnergyScfSubMatcher.copy(), # need copy since SubMatcher already used for ScfInitialization
         SimpleMatcher(r"\s*Self-consistency convergence accuracy:"),
         SimpleMatcher(r"\s*\|\s*Change of charge(?:/spin)? density\s*:\s*[-+0-9.eEdD]+\s*[-+0-9.eEdD]*"),
         SimpleMatcher(r"\s*\|\s*Change of sum of eigenvalues\s*:\s*[-+0-9.eEdD]+ *eV"),
-        SimpleMatcher(r"\s*\|\s*Change of total energy\s*:\s*(?P<energy_change_scf_iteration>[-+0-9.eEdD]+) *eV"),
+        SimpleMatcher(r"\s*\|\s*Change of total energy\s*:\s*(?P<energy_change_scf_iteration__eV>[-+0-9.eEdD]+) *eV"),
         SimpleMatcher(r"\s*\|\s*Change of forces\s*:\s*[-+0-9.eEdD]+ *eV/A"),
         # After convergence eigenvalues are printed in the end instead of usually in the beginning
-        EigenvaluesSubMatcher.copy() # need copy since SubMatcher already used for ScfInitialization
-                    ])
+        EigenvaluesGroupSubMatcher.copy(), # need copy since SubMatcher already used for ScfInitialization
+        SimpleMatcher(r"\s*End self-consistency iteration #\s*[0-9]+\s*:\s*max\(cpu_time\)\s*wall_clock\(cpu1\)"),
+        SimpleMatcher(r"\s*Self-consistency cycle converged\.")
+                    ]),
+      SimpleMatcher(r"\s*Energy and forces in a compact form:"),
+      SimpleMatcher(r"\s*\|\s*Total energy uncorrected\s*:\s*(?P<energy_total>[-+0-9.eEdD]+) *eV"),
+      SimpleMatcher(r"\s*\|\s*Total energy corrected\s*:\s*(?P<energy_total_T0>[-+0-9.eEdD]+) *eV"),
+      SimpleMatcher(r"\s*\|\s*Electronic free energy\s*:\s*(?P<energy_free>[-+0-9.eEdD]+) *eV")
                   ])
                 ])
               ])
@@ -178,32 +203,17 @@ mainFileDescription = SimpleMatcher(name = 'root',
 # loading metadata from nomad-meta-info/meta_info/nomad_meta_info/fhi_aims.nomadmetainfo.json
 metaInfoPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../../../../nomad-meta-info/meta_info/nomad_meta_info/fhi_aims.nomadmetainfo.json"))
 metaInfoEnv, warnings = loadJsonFile(filePath = metaInfoPath, dependencyLoader = None, extraArgsHandling = InfoKindEl.ADD_EXTRA_ARGS, uri = None)
-# set up class for eigenvalues that contains variable names and the regular expression
-evUtility = ParseEigenvaluesNamesAndRe(metaInfoEnv = metaInfoEnv,
-                                       metaNameEvNumber = 'eigenvalues_eigenvalues_number',
-                                       metaNameEvOccupation = 'eigenvalues_occupation',
-                                       metaNameEvValue = 'eigenvalues_eigenvalues')
-
+# dictionary for functions to call on close of a section metaname
 onClose = {}
-
-parserInfo = {"name":"fhi-aims-parser", "version": "1.0"}
-
-class FhiAimsParserContext(object):
-    def __init__(self):
-        self.scfIterNr = 0
-
-    def onClose_section_single_point_evaluation(self, backend, gIndex, section):
-        """trigger called when section_scf_iteration is closed"""
-        #backend.addValue("", self.scfIterNr)
-        logging.getLogger("nomadcore.parsing").info("XXXX bla gIndex %d %s", gIndex, section.simpleValues)
-        self.scfIterNr = 0
-
-    def onClose_section_scf_iteration(self, backend, gIndex, section):
-        """trigger called when section_scf_iteration is closed"""
-        logging.getLogger("nomadcore.parsing").info("YYYY bla gIndex %d %s", gIndex, section.simpleValues)
-        self.scfIterNr += 1
-
-cachingLevelForMetaName = { "eigenvalues_eigenvalues": CachingLevel.Forward }
+# parser info
+parserInfo = {'name':'fhi-aims-parser', 'version': '1.0'}
+# adjust caching of metadata
+cachingLevelForMetaName = {'fhi_aims_section_eigenvalues_list': CachingLevel.Ignore,
+                           'fhi_aims_eigenvalue_occupation': CachingLevel.Cache,
+                           'fhi_aims_eigenvalue_eigenvalue': CachingLevel.Cache,
+                           'eigenvalues_occupation': CachingLevel.Forward,
+                           'eigenvalues_eigenvalues': CachingLevel.Forward,
+                           'scf_dft_number_of_iterations': CachingLevel.Forward}
 
 if __name__ == "__main__":
-    mainFunction(mainFileDescription, metaInfoEnv, parserInfo, superContext = FhiAimsParserContext(), cachingLevelForMetaName = cachingLevelForMetaName)
+    mainFunction(mainFileDescription, metaInfoEnv, parserInfo, superContext = FhiAimsParserContext(), cachingLevelForMetaName = cachingLevelForMetaName, onClose = onClose)
