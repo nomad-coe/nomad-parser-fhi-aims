a1910827f977b69bb37dc749460bfb16838e5dda
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-01-15 16:35:31 +0100

    Completing scala wrapper definition

--- src/main/scala/eu/nomad_lab/parsers/FhiAimsParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/FhiAimsParser.scala
@@ -8,15 +8,19 @@ object FhiAimsParser extends SimpleExternalParserGenerator(
         ("name" -> jn.JString("FhiAimsParser")) ::
           ("version" -> jn.JString("1.0")) :: Nil),
       mainFileTypes = Seq("text/.*"),
-      mainFileRe = """          Invoking FHI-aims ...
-          Version """.r,
-      cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/SimpleFhiParser.py",
+      mainFileRe = """\s*Invoking FHI-aims \.\.\.
+\s*Version """.r,
+      cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/fhi-aims/parser/parser-fhi-aims/SimpleFhiParser.py",
         "--uri", "${mainFileUri}", "${mainFilePath}"),
       resList = Seq(
         "parser-fhi-aims/SimpleFhiParser.py",
-        "parser-fhi-aims/setup_paths.py"
+        "parser-fhi-aims/setup_paths.py",
+        "nomad_meta_info/common.nomadmetainfo.json",
+        "nomad_meta_info/meta_types.nomadmetainfo.json",
+        "nomad_meta_info/fhi_aims.nomadmetainfo.json"
       ) ++ DefaultPythonInterpreter.commonFiles(),
       dirMap = Map(
         "parser-fhi-aims" -> "parsers/fhi-aims/parser/parser-fhi-aims",
-        "nomadcore" -> "python-common/python/nomadcore") ++ DefaultPythonInterpreter.commonDirMapping()
+        "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info",
+        "python" -> "python-common/common/python/nomadcore") ++ DefaultPythonInterpreter.commonDirMapping()
 )
