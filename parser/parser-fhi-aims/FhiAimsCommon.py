d7960aca0bf2459b02b6a12ca267135c491eb27a
Franz Knuth <knuth@fhi-berlin.mpg.de>
2016-02-04 19:42:35 +0100

    adjusted parser ouput to new XC handling in metadata

--- parser/parser-fhi-aims/FhiAimsCommon.py
+++ parser/parser-fhi-aims/FhiAimsCommon.py
@@ -2,7 +2,7 @@ import setup_paths
 import numpy as np
 from nomadcore.local_meta_info import loadJsonFile, InfoKindEl
 from nomadcore.unit_conversion.unit_conversion import convert_unit
-import os, re
+import json, os, re
 
 ############################################################
 # This file contains functions that are needed
@@ -110,7 +110,7 @@ def write_xc_functional(backend, metaInfoEnv, metaNameStart, valuesDict, locatio
     """Function to write xc settings for controlIn and controlInOut.
 
     The omega of the HSE-functional is converted to the unit given in the metadata.
-    The xc functional from controlInOut is converted to the string required for the metadata XC_functional.
+    The xc functional from controlInOut is converted and writen in the format specified in the metadata.
 
     Args:
         backend: Takes care of the writting of the metadata.
@@ -120,27 +120,51 @@ def write_xc_functional(backend, metaInfoEnv, metaNameStart, valuesDict, locatio
         location: A string that is used to specify where more than one setting for xc was found.
         logger: Logging object where messages should be written to.
     """
-    # dictionary for conversion of xc functional name in aims to metadata format
-    # TODO hybrid GGA and mGGA functionals and vdW functionals
+    # two function to convert hybrid_xc_coeff to the correct weight
+    def GGA_weight(x):
+        return 1.0 - x
+    def HF_weight(x):
+        return x
+    # TODO vdW functionals, double-hybrid functionals, screx, and cohsex
+    # Dictionary for conversion of xc functional name in aims to metadata format.
+    # The individual x and c components of the functional are given as dictionaries.
+    # Possible keys of such a dictionary are 'name', 'weight', and 'convert'.
+    # If 'weight' is not given it is not written.
+    # With 'convert', a funtion is specified how hybrid_xc_coeff is converted to the correct weight for this xc component.
     xcDict = {
-              'Perdew-Wang parametrisation of Ceperley-Alder LDA': 'LDA_C_PW_LDA_X',
-              'Perdew-Zunger parametrisation of Ceperley-Alder LDA': 'LDA_C_PZ_LDA_X',
-              'VWN-LDA parametrisation of VWN5 form': 'LDA_C_VWN_LDA_X',
-              'VWN-LDA parametrisation of VWN-RPA form': 'LDA_C_VWN_RPA_LDA_X',
-              'AM05 gradient-corrected functionals': 'GGA_C_AM05_GGA_X_AM05',
-              'BLYP functional': 'GGA_C_LYP_GGA_X_B88',
-              'PBE gradient-corrected functionals': 'GGA_C_PBE_GGA_X_PBE',
-              'PBEint gradient-corrected functional': 'GGA_C_PBEINT_GGA_X_PBEINT',
-              'PBEsol gradient-corrected functionals': 'GGA_C_PBE_SOL_GGA_X_PBE_SOL',
-              'RPBE gradient-corrected functionals': 'GGA_C_PBE_GGA_X_RPBE',
-              'revPBE gradient-corrected functionals': 'GGA_C_PBE_GGA_X_PBE_R',
-              'PW91 gradient-corrected functionals': 'GGA_C_PW91_GGA_X_PW91',
-              'M06-L gradient-corrected functionals': 'MGGA_C_M06_L_MGGA_X_M06_L',
-              'M11-L gradient-corrected functionals': 'MGGA_C_M11_L_MGGA_X_M11_L',
-              'TPSS gradient-corrected functionals': 'MGGA_C_TPSS_MGGA_X_TPSS',
-              'TPSSloc gradient-corrected functionals': 'MGGA_C_TPSSLOC_MGGA_X_TPSS',
-              'Hartree-Fock': 'HF_X',
+              'Perdew-Wang parametrisation of Ceperley-Alder LDA':   [{'name': 'LDA_C_PW'}, {'name': 'LDA_X'}],
+              'Perdew-Zunger parametrisation of Ceperley-Alder LDA': [{'name': 'LDA_C_PZ'}, {'name': 'LDA_X'}],
+              'VWN-LDA parametrisation of VWN5 form':                [{'name': 'LDA_C_VWN'}, {'name': 'LDA_X'}],
+              'VWN-LDA parametrisation of VWN-RPA form':             [{'name': 'LDA_C_VWN_RPA'}, {'name': 'LDA_X'}],
+              'AM05 gradient-corrected functionals':                 [{'name': 'GGA_C_AM05'}, {'name': 'GGA_X_AM05'}],
+              'BLYP functional':                                     [{'name': 'GGA_C_LYP'}, {'name': 'GGA_X_B88'}],
+              'PBE gradient-corrected functionals':                  [{'name': 'GGA_C_PBE'}, {'name': 'GGA_X_PBE'}],
+              'PBEint gradient-corrected functional':                [{'name': 'GGA_C_PBEINT'}, {'name': 'GGA_X_PBEINT'}],
+              'PBEsol gradient-corrected functionals':               [{'name': 'GGA_C_PBE_SOL'}, {'name': 'GGA_X_PBE_SOL'}],
+              'RPBE gradient-corrected functionals':                 [{'name': 'GGA_C_PBE'}, {'name': 'GGA_X_RPBE'}],
+              'revPBE gradient-corrected functionals':               [{'name': 'GGA_C_PBE'}, {'name': 'GGA_X_PBE_R'}],
+              'PW91 gradient-corrected functionals':                 [{'name': 'GGA_C_PW91'}, {'name': 'GGA_X_PW91'}],
+              'M06-L gradient-corrected functionals':                [{'name': 'MGGA_C_M06_L'}, {'name': 'MGGA_X_M06_L'}],
+              'M11-L gradient-corrected functionals':                [{'name': 'MGGA_C_M11_L'}, {'name': 'MGGA_X_M11_L'}],
+              'TPSS gradient-corrected functionals':                 [{'name': 'MGGA_C_TPSS'}, {'name': 'MGGA_X_TPSS'}],
+              'TPSSloc gradient-corrected functionals':              [{'name': 'MGGA_C_TPSSLOC'}, {'name': 'MGGA_X_TPSS'}],
+              'hybrid B3LYP functional':                             [{'name': 'HYB_GGA_XC_B3LYP5'}],
+              'Hartree-Fock':                                        [{'name': 'HF_X'}],
+              'hybrid-PBE0 functionals':                             [{'name': 'GGA_C_PBE'}, {'name': 'GGA_X_PBE', 'weight': 0.75, 'convert': GGA_weight}, {'name': 'HF_X', 'weight': 0.25, 'convert': HF_weight}],
+              'hybrid-PBEsol0 functionals':                          [{'name': 'GGA_C_PBE_SOL'}, {'name': 'GGA_X_PBE_SOL', 'weight': 0.75, 'convert': GGA_weight}, {'name': 'HF_X', 'weight': 0.25, 'convert': HF_weight}],
+              'HSE':                                                 [{'name': 'HYB_GGA_XC_HSE03'}],
+              'HSE-functional':                                      [{'name': 'HYB_GGA_XC_HSE06'}],
+              'Hybrid M06 gradient-corrected functionals':           [{'name': 'HYB_MGGA_XC_M06'}],
+              'Hybrid M06-2X gradient-corrected functionals':        [{'name': 'HYB_MGGA_XC_M06_2X'}],
+              'Hybrid M06-HF gradient-corrected functionals':        [{'name': 'HYB_MGGA_XC_M06_HF'}],
+              'Hybrid M08-HX gradient-corrected functionals':        [{'name': 'HYB_MGGA_XC_M08_HX'}],
+              'Hybrid M08-SO gradient-corrected functionals':        [{'name': 'HYB_MGGA_XC_M08_SO'}],
+              'Hybrid M11 gradient-corrected functionals':           [{'name': 'HYB_MGGA_XC_M11'}],
              }
+    # description for hybrid coefficient
+    xcHybridCoeffDescr = 'hybrid coefficient $\\alpha$'
+    # descritpion for omega of HSE06
+    xcOmegaDescr = '$\\omega$ in m^-1'
     # distinguish between control.in and the aims output from the parsed control.in
     if metaNameStart == 'fhi_aims_controlIn':
         hseFunc = 'hse06'
@@ -156,34 +180,68 @@ def write_xc_functional(backend, metaInfoEnv, metaNameStart, valuesDict, locatio
     if xc is not None:
         # check if only one xc keyword was found in control.in
         if len(xc) > 1:
-            logger.warning("Found %d settings for the xc functional in %s: %s. This leads to an undefined behavior und no metadata can be written for %s_xc." % (len(xc), location, xc, metaNameStart))
+            logger.error("Found %d settings for the xc functional in %s: %s. This leads to an undefined behavior of the calculation and no metadata can be written for %s_xc." % (len(xc), location, xc, metaNameStart))
         else:
             backend.superBackend.addValue(metaNameStart + '_xc', xc[-1])
-            # convert xc functional to metadata XC_functional
-            if xcWrite:
-                xcMetaName = 'XC_functional'
-                xcString = xcDict.get(xc[-1])
-                if xcString is not None:
-                    backend.addValue(xcMetaName, xcString)
-                else:
-                    logger.error("The xc functional '%s' could not be converted to the required string for the metadata '%s'. Please add it to the dictionary xcDict in %s." % (xc[-1], xcMetaName, os.path.basename(__file__)))
             # hse_omega is only written for HSE06
-            hse_omega = valuesDict.get(metaNameStart + '_hse_omega')
-            if hse_omega is not None and xc[-1] == hseFunc:
-                # get unit from metadata
-                unit = metaInfoEnv.infoKinds[metaNameStart + '_hse_omega'].units
-                # try unit conversion and write hse_omega
-                hse_unit = valuesDict.get(metaNameStart + '_hse_unit')
-                if hse_unit is not None:
-                    value = None
-                    if hse_unit[-1] in ['a', 'A']:
-                        value = convert_unit(hse_omega[-1], 'angstrom**-1', unit)
-                    elif hse_unit[-1] in ['b', 'B']:
-                        value = convert_unit(hse_omega[-1], 'bohr**-1', unit)
-                    if value is not None:
-                        backend.superBackend.addValue(metaNameStart + '_hse_omega', value)
+            if xc[-1] == hseFunc:
+                hse_omega = valuesDict.get(metaNameStart + '_hse_omega')
+                if hse_omega is not None:
+                    # get unit from metadata
+                    unit = metaInfoEnv.infoKinds[metaNameStart + '_hse_omega'].units
+                    # try unit conversion and write hse_omega
+                    hse_unit = valuesDict.get(metaNameStart + '_hse_unit')
+                    if hse_unit is not None:
+                        omegaValue = None
+                        if hse_unit[-1] in ['a', 'A']:
+                            omegaValue = convert_unit(hse_omega[-1], 'angstrom**-1', unit)
+                        elif hse_unit[-1] in ['b', 'B']:
+                            omegaValue = convert_unit(hse_omega[-1], 'bohr**-1', unit)
+                        if omegaValue is not None:
+                            backend.superBackend.addValue(metaNameStart + '_hse_omega', omegaValue)
+                        else:
+                            logger.warning("Unknown hse_unit %s in %s. Cannot write %s" % (hse_unit[-1], location, metaNameStart + '_hse_omega'))
                     else:
-                        logger.warning("Unknown hse_unit %s in %s. Cannot write %s" % (hse_unit[-1], location, metaNameStart + '_hse_omega'))
+                        logger.warning("No value found for %s. Cannot write %s" % (metaNameStart + '_hse_unit', metaNameStart + '_hse_omega'))
+            # convert xc functional for metadata
+            if xcWrite:
+                # get list of xc components according to parsed value
+                xcList = xcDict.get(xc[-1])
+                if xcList is not None:
+                    # loop over the xc components
+                    for xcItem in xcList:
+                        xcName = xcItem.get('name')
+                        if xcName is not None:
+                            # write section and and XC_functional_name
+                            gIndex = backend.openSection('section_XC_functionals')
+                            backend.addValue('XC_functional_name', xcName)
+                            # check for hybrid_xc_coeff
+                            hybridCoeff = valuesDict.get(metaNameStart + '_hybrid_xc_coeff')
+                            # write hybrid_xc_coeff for B3LYP and HSE03
+                            if hybridCoeff is not None and xc[-1] in ['hybrid B3LYP functional', 'HSE']:
+                                backend.addValue('XC_functional_parameters', {xcHybridCoeffDescr: hybridCoeff[-1]})
+                            # write omega and hybrid_xc_coeff for HSE06
+                            elif xc[-1] == hseFunc:
+                                # converted value of omega was obtained above
+                                parameters = {xcOmegaDescr: omegaValue}
+                                # add hybrid_xc_coeff
+                                if hybridCoeff is not None:
+                                    hybrid = hybridCoeff[-1]
+                                else:
+                                    hybrid = 0.25
+                                parameters[xcHybridCoeffDescr] = hybrid
+                                backend.addValue('XC_functional_parameters', parameters)
+                            # adjust weight of functionals that are affected by hybrid_xc_coeff
+                            elif hybridCoeff is not None and 'convert' in xcItem:
+                                backend.addValue('XC_functional_weight', xcItem['convert'](hybridCoeff[-1]))
+                            # write weight if present for current xcItem
+                            else:
+                                xcWeight = xcItem.get('weight')
+                                if xcWeight is not None:
+                                    backend.addValue('XC_functional_weight', xcWeight)
+                            backend.closeSection('section_XC_functionals', gIndex)
+                        else:
+                            logger.error("The dictionary for xc functional '%s' does not have the key 'name'. Please correct the dictionary xcDict in %s." % (xc[-1], os.path.basename(__file__)))
                 else:
-                    logger.warning("No value found for %s. Cannot write %s" % (metaNameStart + '_hse_unit', metaNameStart + '_hse_omega'))
+                    logger.error("The xc functional '%s' could not be converted for the metadata. Please add it to the dictionary xcDict in %s." % (xc[-1], os.path.basename(__file__)))
 
