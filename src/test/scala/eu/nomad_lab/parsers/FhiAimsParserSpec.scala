0d0d8e1fde03cf79da304a31bbe75e27ac54b62f
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-02-11 10:09:13 +0100

    Update test

--- src/test/scala/eu/nomad_lab/parsers/FhiAimsParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/FhiAimsParserSpec.scala
@@ -6,8 +6,11 @@ import org.specs2.mutable.Specification
 
 object FhiAimsParserTests extends Specification {
   "FHIAimsParserTest" >> {
-    "test with ...ZORA.out" >> {
+    "test with json-events" >> {
       ParserRun.parse(FhiAimsParser,"parsers/fhi-aims/test/examples/Au2_non-periodic_scalar_ZORA.out","json-events") must_== ParseResult.ParseSuccess
+    }
+    "test with json" >> {
+      ParserRun.parse(FhiAimsParser,"parsers/fhi-aims/test/examples/Au2_non-periodic_scalar_ZORA.out","json") must_== ParseResult.ParseSuccess
     }
   }
 }
