35ecb84f542baef0361602962b57a36c2df2c328
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-03-30 15:15:08 +0200

    scalariform

--- src/main/scala/eu/nomad_lab/parsers/FhiAimsParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/FhiAimsParser.scala
@@ -2,40 +2,42 @@ package eu.nomad_lab.parsers
 
 import eu.nomad_lab
 import eu.nomad_lab.DefaultPythonInterpreter
-import org.{json4s => jn}
-import eu.{nomad_lab => lab}
+import org.{ json4s => jn }
+import eu.{ nomad_lab => lab }
 import scala.collection.breakOut
 
 object FhiAimsParser extends SimpleExternalParserGenerator(
-      name = "FhiAimsParser",
-      parserInfo = jn.JObject(
-        ("name" -> jn.JString("FhiAimsParser")) ::
-          ("parserId" -> jn.JString("FhiAimsParser" + lab.FhiAimsVersionInfo.version)) ::
-          ("versionInfo" -> jn.JObject(
-            ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
-              (lab.FhiAimsVersionInfo.toMap.map{ case (key, value) =>
-                (key -> jn.JString(value.toString))
-              }(breakOut): List[(String, jn.JString)])
-          )) :: Nil
-      ),
-      mainFileTypes = Seq("text/.*"),
-      mainFileRe = """\s*Invoking FHI-aims \.\.\.
+  name = "FhiAimsParser",
+  parserInfo = jn.JObject(
+    ("name" -> jn.JString("FhiAimsParser")) ::
+      ("parserId" -> jn.JString("FhiAimsParser" + lab.FhiAimsVersionInfo.version)) ::
+      ("versionInfo" -> jn.JObject(
+        ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
+          (lab.FhiAimsVersionInfo.toMap.map {
+            case (key, value) =>
+              (key -> jn.JString(value.toString))
+          }(breakOut): List[(String, jn.JString)])
+      )) :: Nil
+  ),
+  mainFileTypes = Seq("text/.*"),
+  mainFileRe = """\s*Invoking FHI-aims \.\.\.
 \s*Version """.r,
-      cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/fhi-aims/parser/parser-fhi-aims/FhiAimsParser.py",
-        "--uri", "${mainFileUri}", "${mainFilePath}"),
-      resList = Seq(
-        "parser-fhi-aims/FhiAimsParser.py",
-        "parser-fhi-aims/FhiAimsCommon.py",
-        "parser-fhi-aims/FhiAimsControlInParser.py",
-        "parser-fhi-aims/FhiAimsBandParser.py",
-        "parser-fhi-aims/FhiAimsDosParser.py",
-        "parser-fhi-aims/setup_paths.py",
-        "nomad_meta_info/common.nomadmetainfo.json",
-        "nomad_meta_info/meta_types.nomadmetainfo.json",
-        "nomad_meta_info/fhi_aims.nomadmetainfo.json"
-      ) ++ DefaultPythonInterpreter.commonFiles(),
-      dirMap = Map(
-        "parser-fhi-aims" -> "parsers/fhi-aims/parser/parser-fhi-aims",
-        "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info",
-        "python" -> "python-common/common/python/nomadcore") ++ DefaultPythonInterpreter.commonDirMapping()
+  cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/fhi-aims/parser/parser-fhi-aims/FhiAimsParser.py",
+    "--uri", "${mainFileUri}", "${mainFilePath}"),
+  resList = Seq(
+    "parser-fhi-aims/FhiAimsParser.py",
+    "parser-fhi-aims/FhiAimsCommon.py",
+    "parser-fhi-aims/FhiAimsControlInParser.py",
+    "parser-fhi-aims/FhiAimsBandParser.py",
+    "parser-fhi-aims/FhiAimsDosParser.py",
+    "parser-fhi-aims/setup_paths.py",
+    "nomad_meta_info/common.nomadmetainfo.json",
+    "nomad_meta_info/meta_types.nomadmetainfo.json",
+    "nomad_meta_info/fhi_aims.nomadmetainfo.json"
+  ) ++ DefaultPythonInterpreter.commonFiles(),
+  dirMap = Map(
+    "parser-fhi-aims" -> "parsers/fhi-aims/parser/parser-fhi-aims",
+    "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info",
+    "python" -> "python-common/common/python/nomadcore"
+  ) ++ DefaultPythonInterpreter.commonDirMapping()
 )
--- src/test/scala/eu/nomad_lab/parsers/FhiAimsParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/FhiAimsParserSpec.scala
@@ -2,15 +2,13 @@ package eu.nomad_lab.parsers
 
 import org.specs2.mutable.Specification
 
-
-
 object FhiAimsParserTests extends Specification {
   "FHIAimsParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(FhiAimsParser,"parsers/fhi-aims/test/examples/Au2_non-periodic_geometry_optimization.out","json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(FhiAimsParser, "parsers/fhi-aims/test/examples/Au2_non-periodic_geometry_optimization.out", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(FhiAimsParser,"parsers/fhi-aims/test/examples/Au2_non-periodic_geometry_optimization.out","json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(FhiAimsParser, "parsers/fhi-aims/test/examples/Au2_non-periodic_geometry_optimization.out", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
