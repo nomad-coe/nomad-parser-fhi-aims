Fixed issue with normalizing spin-polarized BS.

fhiaimsparser/fhiaims_parser.py
@@ -732,7 +732,7 @@ class FHIAimsParser(FairdiParser):
                 # been shifted to the fermi energy. This shift is undone so
                 # that the energy scales for for energy_reference_fermi, band
                 # energies and the DOS energies match.
-                sec_k_band_segment.band_energies = pint.Quantity(np.transpose(data[5::2]) + energy_fermi_ev, 'eV')
+                sec_k_band_segment.band_energies = pint.Quantity(np.transpose(data[5::2]) + energy_fermi_ev[:, None, None], 'eV')
 
         def read_dos(dos_file):
             dos_file = self.get_fhiaims_file(dos_file)

tests/test_fhiaimsparser.py
@@ -148,7 +148,7 @@ def test_band_silicon(silicon):
     # reference energy
     energies = energies.flatten()
     energies.sort()
-    lowest_unoccupied_index = np.searchsorted(energies, energy_reference, "right")
+    lowest_unoccupied_index = np.searchsorted(energies, energy_reference, "right")[0]
     highest_occupied_index = lowest_unoccupied_index - 1
     gap = energies[lowest_unoccupied_index] - energies[highest_occupied_index]
     assert gap == pytest.approx(0.60684)
@@ -172,7 +172,7 @@ def test_dos_silicon(silicon):
     nonzero = np.unique(values.nonzero())
     energies = energies[nonzero]
     energies.sort()
-    lowest_unoccupied_index = np.searchsorted(energies, energy_reference, "right")
+    lowest_unoccupied_index = np.searchsorted(energies, energy_reference, "right")[0]
     highest_occupied_index = lowest_unoccupied_index - 1
     gap = energies[lowest_unoccupied_index] - energies[highest_occupied_index]
     assert gap == pytest.approx(0.54054054)